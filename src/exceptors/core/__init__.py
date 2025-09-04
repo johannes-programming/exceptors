ea46e7185871335caa269dff774791d7f565be47
Johannes <johannes.programming@gmail.com>
2025-09-04 22:44:42 +0200

    Version 0.1.3

--- src/exceptors/core/__init__.py
+++ src/exceptors/core/__init__.py
@@ -6,21 +6,18 @@ __all__ = ["Exceptor"]
 
 class Exceptor:
 
-    captured: Optional[type]
+    __slots__ = ("captured",)
+
+    captured: Optional[BaseException]
 
     def __init__(self: Self) -> None:
         self.captured = None
 
     @contextmanager
     def capture(self: Self, *args: type) -> Generator:
-        excTypes: tuple
-        if args == ():
-            excTypes = (BaseException,)
-        else:
-            excTypes = args
         try:
             yield self
-        except excTypes as e:
+        except args as e:
             self.captured = e
         else:
             self.captured = None
