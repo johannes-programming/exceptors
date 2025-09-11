93e62f6e3377e58a95b72d330e9547cde26f19db
Johannes <johannes.programming@gmail.com>
2025-09-11 15:40:53 +0200

    Version 0.2.1

--- src/exceptors/core/__init__.py
+++ src/exceptors/core/__init__.py
@@ -1,29 +1,12 @@
-from contextlib import contextmanager
 from typing import *
+
+from catchlib import Catcher
 
 __all__ = ["Exceptor"]
 
 
-class Exceptor:
+class Exceptor(Catcher): ...
 
-    __slots__ = ("_captured",)
 
-    captured: Optional[BaseException]
-
-    def __init__(self: Self) -> None:
-        self._captured = None
-
-    @contextmanager
-    def capture(self: Self, *args: type) -> Generator:
-        "This contextmanager captures exceptions."
-        try:
-            yield self
-        except args as e:
-            self._captured = e
-        else:
-            self._captured = None
-
-    @property
-    def captured(self: Self) -> property:
-        "This property stores the captured exception."
-        return self._captured
+Exceptor.capture = Exceptor.catch
+Exceptor.captured = Exceptor.caught
